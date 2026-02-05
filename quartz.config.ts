0bb9205f1813e9e50a190c74641838f2ee500fb0
y754 <freyanye@gmail.com>
2026-02-05 17:15:28 +0000

    Quartz sync: Feb 5, 2026, 5:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "City of Doors",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Spectral SC",
+        body: "Spectral",
         code: "IBM Plex Mono",
       },
       colors: {
