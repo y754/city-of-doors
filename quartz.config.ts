d049f495df09e116d4f39df36c6b7e038652f817
y754 <freyanye@gmail.com>
2026-02-05 17:37:24 +0000

    Quartz sync: Feb 5, 2026, 5:37 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "y754.github.io/city-of-doors",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
