6aa42a63be62ca83d7ec15ffd6be79ba314867ee
y754 <freyanye@gmail.com>
2026-02-05 23:03:27 +0000

    Quartz sync: Feb 5, 2026, 11:03 PM

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -23,6 +23,7 @@ import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
 import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
+import RenderBanner from "./RenderBanner"
 
 export {
   ArticleTitle,
