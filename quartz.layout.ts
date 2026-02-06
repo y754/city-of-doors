63d4122fe602c439299bfeb1915774d01d4ff64a
y754 <freyanye@gmail.com>
2026-02-06 09:56:26 +0000

    Quartz sync: Feb 6, 2026, 9:56 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,6 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
+  beforeBody: [Component.RenderBanner()],
   afterBody: [],
   footer: Component.Footer({
     links: {
