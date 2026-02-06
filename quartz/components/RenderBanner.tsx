4ab9924c204083c8ea84bec8fa64b8995be4f976
y754 <freyanye@gmail.com>
2026-02-06 08:59:35 +0000

    Quartz sync: Feb 6, 2026, 8:59 AM

--- quartz/components/RenderBanner.tsx
+++ quartz/components/RenderBanner.tsx
@@ -3,17 +3,30 @@ import { classNames } from "../util/lang"
 
 const RenderBanner: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const banner = fileData.frontmatter?.banner
+  const bannerCitation = fileData.frontmatter?.bannerCitation
   if (banner) {
-    return <img class={classNames(displayClass, "banner")} src={banner} />
+    return <div class="banenrContainer">"<img class={classNames(displayClass, "banner")} src={banner} /><span class="bannerCite">{bannerCitation}</span></div>
   } else {
     return null
   }
 }
 
 RenderBanner.css = `
-.banner {
+.bannerContainer {
   margin: 2rem 0 0 0;
   width: 100%;
+  padding: 0;
+  height: 250px;
+}
+.banner {
+  mask-image: linear-gradient(to bottom, rgb(0, 0, 0),rgba(0,0,0,0));
+}
+.bannerCitation {
+  position: relative;
+  right: 0vw;
+  bottom: 0vh;
+  text-align: right;
+  font-size: 0.8em;
 }
 `
 
